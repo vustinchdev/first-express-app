36eadbb8e86b6d43c52cb1134e8a7216443b6a0d
vustinchdev <vustinchdev@gmail.com>
2024-06-04 15:28:38 +0300

    feat: delete addresses

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import bodyParser from 'body-parser'
 import express from 'express'
 import { productsRouter } from './routes/products-router'
-import { addressesRouter } from './routes/addresses-router'
+
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000
 app.use(bodyParser.json())
 
 app.use('/products', productsRouter)
-app.use('/addresses', addressesRouter)
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
